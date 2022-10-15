724f8d9094226e80a61396cd26e1f24d8930c12f
Michael Simkin <michael.simkin22@gmail.com>
2022-10-15 21:15:05 +0300

    :sparkles: 3D Logo Setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { GUI } from "dat.gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const stats = Stats();
 document.body.appendChild(stats.dom);
@@ -12,15 +12,22 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-const fov = 75;
-const aspect = window.innerWidth / window.innerHeight;
-const near = 0.1;
-const far = 100;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+const top = 3;
+const right = (window.innerWidth / window.innerHeight) * top;
+const camera = new THREE.OrthographicCamera(-right, right, top, -top, 0.1, 100);
+camera.position.z = 10;
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(-1, 1, 2);
+scene.add(ambientLight);
+scene.add(directionalLight);
+
+scene.background = new THREE.Color(0xffffff);
 
 const onWindowResize = () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.right = (window.innerWidth / window.innerHeight) * top;
+    camera.left = -camera.right;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     render();
@@ -28,29 +35,28 @@ const onWindowResize = () => {
 window.addEventListener("resize", onWindowResize, false);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const loader = new GLTFLoader();
 
-const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.scale, "x", 0, 5, 0.1);
-cubeFolder.add(cube.scale, "y", 0, 5, 0.1);
-cubeFolder.add(cube.scale, "z", 0, 5, 0.1);
-cubeFolder.open();
-const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(camera.position, "z", 0, 10);
-cameraFolder.open();
+let logo: THREE.Object3D;
 
-const animate: FrameRequestCallback = (time) => {
-    const seconds = time / 1000;
+const init = async () => {
+    const gltf = await loader.loadAsync("/textures/drive.glb");
+    logo = gltf.scene.children[0];
+    logo.rotation.set(-Math.atan(Math.sqrt(2) / 2), -Math.PI / 4, Math.PI / 2);
+    scene.add(logo);
+};
 
-    cube.rotation.x = cube.rotation.y = cube.rotation.z = seconds;
+const clock = new THREE.Clock();
+
+const animate: FrameRequestCallback = () => {
+    const delta = new THREE.Vector3(0, 0, 10).sub(camera.position);
+    const dt = clock.getDelta();
+    const multiplier = 2 * dt > 0.04 ? 0.04 : 2 * dt;
+    const moveBy = delta.length() > 0.01 ? delta.multiplyScalar(multiplier) : delta;
+    camera.position.add(delta.length() > 1 ? moveBy : delta);
 
     controls.update();
 
@@ -65,4 +71,6 @@ const render = () => {
     renderer.render(scene, camera);
 };
 
-requestAnimationFrame(animate);
+init()
+    .then(() => requestAnimationFrame(animate))
+    .catch(console.log);
